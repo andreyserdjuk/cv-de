4a2b0445e5876e7e0e00361d5911eb243f187db9
Andrii Serdiuk <andrii.serdiuk@instafreight.com>
2024-03-31 16:34:51 +0200

    cv updated

--- src/build.js
+++ src/build.js
@@ -26,7 +26,7 @@ const html = template({
   ...templateData,
   baseUrl: `https://${username()}.github.io/${repoName.sync()}`,
   pdfFileName,
-  updated: dayjs().format('MMMM D, YYYY'),
+  updated: dayjs().format('YYYY.MM.DD'),
 });
 
 fs.writeFileSync(outputDir + '/index.html', html);
--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,65 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
-  facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
-                </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
-  },
+  name: 'Andrii Serdiuk',
+  title: 'Senior Software Engineer',
+  subtitle: 'at <a href="https://www.instafreight.de/en">Instafreight GmbH</a>',
+  facts: [
+    `<a href="https://www.linkedin.com/in/andrii-serdiuk/"><i class="fa-brands bi-linkedin fact-icon"></i>linkedin.com/in/andrii-serdiuk/</a>`,
+    `<a href="https://github.com/andreyserdjuk"><i class="fa-brands bi-github fact-icon"></i>github.com/andreyserdjuk</a>`,
+    '<a href="mailto:ase6120@gmail.com"><i class="fa bi-envelope fact-icon"></i>ase6120@gmail.com</a>',
+    // '<a href="mailto:ase6120@gmail.com"><i class="fa bi-phone fact-icon"></i>0178 261 66 11</a>',
+  ],
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 60],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 80],
-    ['Literature', 100],
-    ['Influence', 100],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
+      title: 'Senior Software Engineer',
+      company: 'Instafreight GmbH',
+      period: '2021-2024',
+      skills: ['PHP', 'Symfony Framework', 'PostgreSQL', 'React.js', 'Phalcon Framework', 'Nginx', 'Apache Kafka', 'AWS'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+Implementing new features, decomposition legacy monolytic system to small microservices.
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
+      title: 'Senior Full Stack Engineer',
+      company: 'Evolvice Team GmbH',
+      period: '2019-2020 ',
+      skills: ['PHP', 'Symfony Framework', 'MySQL', 'Nginx', 'Elasticsearch', 'Vagrant', 'Docker', 'Redis', 'Angular', 'Apache Kafka'],
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+Backend: develop and support GraphQL API’s in multi-microservice SMM activity project.  
+Front-end: develop single-page application on Angular 6+, rxjs, ngrx, Ramda, angular-bootstrap.
 `
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: 'Full-stack Developer',
+      company: 'Intersog',
+      period: '2017-2019',
+      skills: ['PHP 7', 'AWS (SQS, SNS)', 'Docker', 'RabbitMQ', 'MySQL', 'Java 8 (Jetty, Hibernate, Maven)', 'Docker'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+Supporting media-exchange platform built with multiple microservices based on PHP, NodeJS, Python and Java. Writing integration workers.
 `
+    },
+    {
+      title: 'Senior PHP Developer',
+      company: 'Netvertise Ltd.',
+      period: '2015-2017',
+      skills: ['PHP', 'Symfony Framework', 'MySQL', 'Nginx', 'Docker'],
+      contents: `
+  Development on Symfony 2 and Phalconphp-based systems, which perform data analysis, payments, parcel delivery.
+  `
+    },
+    {
+      title: 'PHP Programmer',
+      company: 'Autolux',
+      period: '2013-2015',
+      skills: ['PHP5.3', 'MySQL', 'Nginx'],
+      contents: `
+  Full-cycle development of a shipment delivery system and logistic solutions for it.
+  I wrote service REST API using Doctrine ORM, service which controlled replication of data (for our own replication solution) and bridging of data between different systems, etc.
+  Work with Redmine, Git, Bash (wrote scripts), Linux, FreeBSD, PHPUnit etc. Development of deployment scripts.
+  `
     }
   ],
   experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
-    },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
   ]
 };
